2011ec65aba566495dbdafbaf960ed1eb880be0b
Michelle <michelle.douma@gmail.com>
2023-07-09 19:26:32 -0400

    Updates

---
+++ precache-manifest.7c6127da3e47c728a90cbf09c519c5bd.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc5ac4f620a47a2ae9e7",
+    "url": "/Amuodmi/MyPortfolio/static/css/main.692a4472.chunk.css"
+  },
+  {
+    "revision": "cc5ac4f620a47a2ae9e7",
+    "url": "/Amuodmi/MyPortfolio/static/js/main.cc5ac4f6.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/Amuodmi/MyPortfolio/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "21555fbbe2215e9210ae",
+    "url": "/Amuodmi/MyPortfolio/static/js/2.21555fbb.chunk.js"
+  },
+  {
+    "revision": "6c2e7ae55805748e603f",
+    "url": "/Amuodmi/MyPortfolio/static/js/runtime~main.6c2e7ae5.js"
+  },
+  {
+    "revision": "a48340e3421bc22601ebc50ccd8dbe44",
+    "url": "/Amuodmi/MyPortfolio/static/media/profilePhoto.a48340e3.jpg"
+  },
+  {
+    "revision": "b4ba25b2968a4ecdd240de0ccca11ece",
+    "url": "/Amuodmi/MyPortfolio/static/media/giftyouUp.b4ba25b2.png"
+  },
+  {
+    "revision": "70250188fdb832be9692fd9ce3a09f8e",
+    "url": "/Amuodmi/MyPortfolio/static/media/musicboximage.70250188.png"
+  },
+  {
+    "revision": "139f60978b2fc6c46c9abbb1883d6e9e",
+    "url": "/Amuodmi/MyPortfolio/static/media/Michelle_Douma_Resume_2023.139f6097.pdf"
+  },
+  {
+    "revision": "1e36264dbc2d407bce305b1eec3d4b46",
+    "url": "/Amuodmi/MyPortfolio/static/media/bash.1e36264d.svg"
+  },
+  {
+    "revision": "15282dff001376d035d9167b7633cb69",
+    "url": "/Amuodmi/MyPortfolio/static/media/bootstrap.15282dff.svg"
+  },
+  {
+    "revision": "84ccd1df33a9a0a1a6852b30fd8edaf8",
+    "url": "/Amuodmi/MyPortfolio/static/media/bulma.84ccd1df.svg"
+  },
+  {
+    "revision": "7c6a6b602f10297741009c4a4ede317c",
+    "url": "/Amuodmi/MyPortfolio/static/media/css.7c6a6b60.svg"
+  },
+  {
+    "revision": "8260c0cc847a9414de1325084f425404",
+    "url": "/Amuodmi/MyPortfolio/static/media/express.8260c0cc.svg"
+  },
+  {
+    "revision": "c7ef676648d0a63907b26420105ed20d",
+    "url": "/Amuodmi/MyPortfolio/static/media/figma.c7ef6766.svg"
+  },
+  {
+    "revision": "fef8e95112e6ab6f2169430ee510db5e",
+    "url": "/Amuodmi/MyPortfolio/static/media/git.fef8e951.svg"
+  },
+  {
+    "revision": "6ec4ebfc8814a9a0f2825ee7b8e18da0",
+    "url": "/Amuodmi/MyPortfolio/static/media/graphql.6ec4ebfc.svg"
+  },
+  {
+    "revision": "3730792cdd49b256a588906008e5e5e7",
+    "url": "/Amuodmi/MyPortfolio/static/media/handlebars.3730792c.svg"
+  },
+  {
+    "revision": "7aa5faf85303e318c9388b97b62bf2f4",
+    "url": "/Amuodmi/MyPortfolio/static/media/heroku.7aa5faf8.svg"
+  },
+  {
+    "revision": "de8c501fb8405fd15106c59feab7ad1e",
+    "url": "/Amuodmi/MyPortfolio/static/media/jest.de8c501f.svg"
+  },
+  {
+    "revision": "29614680c75d411ee61bb2d4ecedcd94",
+    "url": "/Amuodmi/MyPortfolio/static/media/jquery.29614680.svg"
+  },
+  {
+    "revision": "8aad4da2ed641d2378923f85fe35189d",
+    "url": "/Amuodmi/MyPortfolio/static/media/mongodb.8aad4da2.svg"
+  },
+  {
+    "revision": "9a87849cee36242dc6ab7399078e0869",
+    "url": "/Amuodmi/MyPortfolio/static/media/mysql.9a87849c.svg"
+  },
+  {
+    "revision": "d5e1cd8cbfe460c028711921565399d6",
+    "url": "/Amuodmi/MyPortfolio/static/media/nodejs.d5e1cd8c.svg"
+  },
+  {
+    "revision": "4f81f37c8c72270f6c343b8814c10ea8",
+    "url": "/Amuodmi/MyPortfolio/static/media/react.4f81f37c.svg"
+  },
+  {
+    "revision": "cd3c60c0cf6524a62662420239f5ea89",
+    "url": "/Amuodmi/MyPortfolio/static/media/redux.cd3c60c0.svg"
+  },
+  {
+    "revision": "d62f2679ff74b4c4cd82deef4f48e361",
+    "url": "/Amuodmi/MyPortfolio/static/media/sequelize.d62f2679.svg"
+  },
+  {
+    "revision": "062dc28833fbd83cbd3f1bc427f3ad15",
+    "url": "/Amuodmi/MyPortfolio/static/media/beach.062dc288.jpeg"
+  },
+  {
+    "revision": "d70501caf717ba33549ee16c2bb2dc44",
+    "url": "/Amuodmi/MyPortfolio/index.html"
+  }
+];
