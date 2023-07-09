85c379f893ef94cabd81e272b3487cc2421bbbf2
Michelle <michelle.douma@gmail.com>
2023-07-09 14:36:04 -0400

    working on Skills page

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,7 +1,18 @@
 import React from "react";
+import JS from "../../images/skillsImage/JSsymbol.svg";
+import Bash from "../../images/skillsImage/bash.svg";
 
 function Skills() {
-	return <div></div>;
+	return (
+		<div className="skillsLayout">
+			<img src={JS} alt="Javascript Logo">
+				Javascript
+			</img>
+			<img src={Bash} alt="Bash Logo">
+				Bash
+			</img>
+		</div>
+	);
 }
 
 export default Skills;
